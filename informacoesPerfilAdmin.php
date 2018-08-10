e41e3e6bce56b0a79296da2f47dd48879b700f47
Letícia Caroline <caroline.ewok@gmail.com>
2018-08-10 10:53:30 -0300

    Add files via upload

---
+++ informacoesPerfilAdmin.php
@@ -0,0 +1,47 @@
+<?php require_once "indexperfil.php"; ?>
+
+
+
+<div id="page-wrapper">
+    <div id="page-inner" class="container">
+
+    <!-- Page Content -->
+
+    <div class="col-md-6 col-sm-4" style="margin-left: 250px; margin-top: 100px">
+        <div class="panel panel-primary" >
+            <div class="panel-heading">
+                Informações
+            </div>
+            <div class="panel-footer">
+                Nome:  Arildinho Maia
+            </div>
+            <div class="panel-footer">
+                Email: arildinho@gmail.com
+            </div>
+            <div class="panel-footer">
+                Telefone:  3070-0987
+            </div>
+            <div class="panel-footer">
+                CNPJ:  59233758000112
+            </div>
+            <div class="panel-footer">
+                Razão Social:  Arildinho Estoque
+            </div>
+            <div class="panel-footer">
+                Nome Fantasia:  Cotton & Cia
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+<!-- /. PAGE WRAPPER  -->
+</div>
+
+
+<?php require_once "rodape.php"; ?>
+
+
+
